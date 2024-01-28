50f80ff8c2f1ac9252f88e9a17e2b3995832783c
PashPush <greentoryrock@gmail.com>
2024-01-28 19:01:20 +0400

    fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='ru'>
-      <body className='grey-gradient flex h-full items-center justify-center md:h-screen'>
+      <body className='grey-gradient flex h-full items-center justify-center md:h-screen '>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Toaster } from '@/components/ui/toaster'
 export default function Home() {
   return (
     <main className='flex py-6 text-white'>
-      <div className='mx-auto min-w-[400px] max-w-7xl px-6'>
+      <div className='mx-auto min-w-[360px] max-w-7xl px-6'>
         <Columns />
         <Toaster />
       </div>
